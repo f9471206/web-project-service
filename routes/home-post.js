e53ddb779eccb29272f8cb6223cdac80439dd24f
f9471206 <f9471206@gmail.com>
2023-12-21 19:27:09 +0800

    updata show user data

--- routes/home-post.js
+++ routes/home-post.js
@@ -16,13 +16,24 @@ router.get("/", async (req, res) => {
   res.send(getPost);
 });
 
+//user data
+router.get("/userData/:_id", async (req, res) => {
+  let { _id } = req.params;
+  try {
+    let data = await postModel.find({ author: _id }).populate("like").exec();
+    res.send(data);
+  } catch (e) {
+    res.send(e);
+  }
+});
+
 //search post
 router.get("/:_id", async (req, res) => {
   let { _id } = req.params;
   try {
     let searchPost = await postModel
       .findOne({ _id })
-      .populate("author", ["username", "photo"])
+      .populate("author", ["username", "photo", "_id"])
       .populate("reply.user", ["username", "photo"])
       .exec();
     res.send(searchPost);
